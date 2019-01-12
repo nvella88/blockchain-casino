2c7b249110cc04934f4d0e04ce78ad5da2f875b1
Nicholas <nicholas@bluewindowltd.com>
2019-01-12 11:43:13 +0100

    Add self destruct and fallback function

--- contracts/Roulette.sol
+++ contracts/Roulette.sol
@@ -1,16 +1,40 @@
 pragma solidity ^0.4.24;
 
 contract Roulette{
-    // The croupier is the creator of the smart cotract instance.
+    // The croupier is the creator/owner of the smart contract.
     address croupier;
     // The roulette gas a fixed stake set by the croupier.
     uint256 tableStake;
     bool isTableOpen = false;
+
+    // Function modifier which requires the caller 
+    // of the function to be the owner of the contract.
+    // Source: https://solidity.readthedocs.io/en/v0.5.1/structure-of-a-contract.html?highlight=modifiers#structure-function-modifiers
+    modifier ownerRequired {
+        require(
+            msg.sender == croupier,
+            "Only owner can call this function."
+        );
+        _;
+    }
 
     constructor(uint256 stakeAmount) public
     {
         croupier = msg.sender;
         tableStake = stakeAmount;
         isTableOpen = true;
+    }
+
+    // Closes the table, destroying the smart contract.
+    // Only the creator of the 
+    function closeTable() public ownerRequired {
+        // Convert croupier frm address to address payable.
+        selfdestruct(address(uint160(croupier)));
+    }
+
+    // The fallback function is used when the smart contract is called 
+    // with a function identifier which does not exist.
+    function() external {
+        revert("This function is not allowed. Try another one.");
     }
 }
