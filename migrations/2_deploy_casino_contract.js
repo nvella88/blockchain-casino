2c8e1f46a467a549ab20abd87128ccb12caf9b99
Nicholas <nicholas@bluewindowltd.com>
2019-01-27 09:00:39 +0100

    Started working on js file

--- migrations/2_deploy_casino_contract.js
+++ migrations/2_deploy_casino_contract.js
@@ -4,7 +4,5 @@ module.exports = function(deployer) {
   // Deploying the smart contract named roulette, with a stake value of 10.
   // The first address in the local etherium network will be used,
   // making the first address as the owner.
-  // For learning purposes, we are using this level of granularity
-  // to not allow Truffle do a lot of hidden setup on its own.
   deployer.deploy(Roulette, 10, {from:web3.eth.accounts[0]});
 };
