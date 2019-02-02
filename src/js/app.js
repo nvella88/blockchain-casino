6cd4bc131ecb14ad1eaafcedf6b13422148a7e26
Nicholas <nicholas@bluewindowltd.com>
2019-02-02 12:26:28 +0100

    Make front end work

--- src/js/app.js
+++ src/js/app.js
@@ -3,11 +3,11 @@ App = {
   contracts: {},
   // The addresses on the network will be retrieved once.
   // This setup will make use of more then one address.
-  accounts,
+  accounts: null,
   // Croupier account (accounts[0]) will be assigned to a variable. It will be easier to follow.
   // The rest of the accounts will be read from the array, accounts[1], and so on.
-  croupierAccount,
-  tableStake,
+  croupierAccount: null,
+  tableStake: 0,
 
   init: async function () {
     return await App.initWeb3();
@@ -18,8 +18,7 @@ App = {
     // A more realistic scenario would be to load Metamask and connect it to the Etherium network of choice.
     // In this case the development network will always be used.
     // Reference: https://truffleframework.com/tutorials/pet-shop
-    App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-    web3 = new Web3(App.web3Provider);
+    web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545'));    
     return App.initContract();
   },
 
@@ -28,12 +27,11 @@ App = {
       // Get the necessary contract artifact file and instantiate it with truffle-contract
       var RouletteArtifact = data;
       App.contracts.Roulette = TruffleContract(RouletteArtifact);
-
       // Set the provider for our contract
-      App.contracts.Roulette.setProvider(App.web3Provider);
-    });
+      App.contracts.Roulette.setProvider(web3.currentProvider);
 
-    return App.loadAccounts();
+      return App.loadAccounts();
+    });    
   },
 
   // Get accounts and assign the values to this instance.
