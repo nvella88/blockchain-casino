8828830a450b5284135c27e1631f87b41f77ebd6
Nicholas <nicholas@bluewindowltd.com>
2019-02-11 12:37:37 +0100

    Remove .sol tests and document .js tests

--- test/TestRoulette.js
+++ test/TestRoulette.js
@@ -3,32 +3,43 @@ const Roulette = artifacts.require("Roulette");
 contract("Roulette", accounts => {
     // Truffle deploys the smart contract using the first address by default.
     // This makes the first account the croupier.
-    // This scenario is covered in TestRoulette.sol.
     const [firstAccount, secondAccount, thirdAccount, fourthAccount] = accounts;
     let roulette;
 
     // Common setup before executing each test.
+    // Creating a new instance before each test is expensive and running these tests too many times will quickly deplete ether.
+    // The reason a new instance is created is to ensure atomicity of every test, 
+    // that is the behaviour of a test is not affected by the execution of others.
     beforeEach('setup contract for each test', async function () {
         roulette = await Roulette.new(10)
     })
 
+    // Ensures that the correct stake value is returned.
+    // The stake value is displayed on the front-end part.
     it("should return the correct table stake value", async () => {
         var result = await roulette.getTableStake();
         assert(result, 10);
     });
 
+    // Checks if a player session has been created.
+    // A player session is the interaction (in this case bets) a user made on this smart contract.
+    // The bets are stored and the necessary calculations are done after the number is drawn.
     it("should not create a session if a player has never placed a bet", async () => {
         var result = await roulette.isPlayerSessionCreated(secondAccount, { from: firstAccount });
         assert(result === false);
     });
 
+    // Checks if a player session has been created.
+    // A player session is the interaction (in this case bets) a user made on this smart contract.
+    // The bets are stored and the necessary calculations are done after the number is drawn.
     it("should create a session if a user placed a bet", async () => {
         await roulette.betOnOddNumber({ from: secondAccount, value: 10 });
         var result = await roulette.isPlayerSessionCreated(secondAccount, { from: firstAccount });
         assert(result === true);
     });
 
-    // Bet placement logic, that is, testing requires.
+    // Ensure the croupier cannot place bets.
+    // The idea is to simulate 'fairness' in which the house cannot place bets against itself.
     // Source: https://medium.com/@gus_tavo_guim/testing-your-smart-contracts-with-javascript-40d4edc2abed
     it("should not allow croupier to place a bet", async () => {
         try {
@@ -39,6 +50,7 @@ contract("Roulette", accounts => {
         }
     });
 
+    // All bets need to match the table stake.
     it("should not allow bets which do not match stake", async () => {
         try {
             await roulette.betOnOddNumber({ from: secondAccount, value: 11 });
@@ -48,6 +60,7 @@ contract("Roulette", accounts => {
         }
     });
 
+    // A user cannot bet twice on the same bet.
     it("should not allow duplicte bets", async () => {
         // Valid bet
         await roulette.betOnOddNumber({ from: secondAccount, value: 10 });
@@ -61,6 +74,7 @@ contract("Roulette", accounts => {
         }
     });
 
+    // A user cannot bet on opposing bets.
     it("should not allow betting on both odd and even numbers", async () => {
         // Valid bet: Odd
         await roulette.betOnOddNumber({ from: secondAccount, value: 10 });
@@ -74,6 +88,8 @@ contract("Roulette", accounts => {
         }
     });
 
+    // When a table closes, bets are not allowed.
+    // The idea behind closing a table is to give time to all transactions to be mined before drawing the number.
     it("should not allow betting when the table is closed for betting", async () => {
         await roulette.closeBets({ from: firstAccount });
 
@@ -86,6 +102,7 @@ contract("Roulette", accounts => {
         }
     });
 
+    // The winning number should be set and read correctly.
     it("should set the winning number correctly", async () => {
         await roulette.closeBets({ from: firstAccount });
         await roulette.setWinningNumber(5);
@@ -95,6 +112,8 @@ contract("Roulette", accounts => {
 
     });
 
+    // The table must be closed to set a winning number.
+    // This is used to make sure that users cannot bet while the winning number is set.
     it("should not set the winning number if bets are allowed", async () => {
         try {
             await roulette.setWinningNumber(5);
@@ -104,6 +123,7 @@ contract("Roulette", accounts => {
         }
     });
 
+    // Roulettes allow numbers between 0 and 36.
     it("should not set the winning number if input is invalid", async () => {
         await roulette.closeBets({ from: firstAccount });
         try {
@@ -114,6 +134,8 @@ contract("Roulette", accounts => {
         }
     });
 
+    // A winning number cannot be set twice.
+    // This is a preventative measure to promote fairness.
     it("should not set the winning number twice", async () => {
         await roulette.closeBets({ from: firstAccount });
         await roulette.setWinningNumber(5);
@@ -125,7 +147,10 @@ contract("Roulette", accounts => {
         }
     });
 
-    it("should allow winning accounts to withdraw", async () => {
+    // The correct withdrawable balance should be provided.
+    // The test was written to cover one of the most important functionalities of the contract, 
+    // that is, to ensure that the winners and their winning amount are calculated correctly.
+    it("should provide the correct withdrawable balance", async () => {
         // Place some bets, close betting and set a winning number.
         await roulette.betOnOddNumber({ from: secondAccount, value: 10 });
         await roulette.betOnEvenNumber({ from: thirdAccount, value: 10 });
@@ -142,6 +167,8 @@ contract("Roulette", accounts => {
         assert.equal(resultFourthAccount.toString(), 0)
     });
 
+    // Users are not allowed to withdraw winnings twice.
+    // The test covers an important piece of logic in which a user can exploit the system and withdraw the winnings twice.
     it("should not allow to withdraw winnings twice", async () => {
         // Place some bets, close betting and set a winning number.
         await roulette.betOnOddNumber({ from: secondAccount, value: 10 });
@@ -160,6 +187,8 @@ contract("Roulette", accounts => {
         }
     });
 
+    // Users not eligible for winnings are not allowed to withdraw.
+    // The test was written to ensure that only eligible users are able to withdraw winnings.
     it("should not allow losing accounts to withdraw", async () => {
         // Place some bets, close betting and set a winning number.
         await roulette.betOnOddNumber({ from: secondAccount, value: 10 });
